870eb3128be06d0c65c9a335067c5cc2004ed28f
Mrinmoydatta <147488441+Mrinmoydatta@users.noreply.github.com>
2023-11-22 15:16:18 +0530

    #include<stdio.h> int main() {     int n,sq;     //scanf("%d",&n);     while(n!=-1)     {         scanf("%d",&n);         if(n!=-1)         {         sq=n*n;         printf("%d ",sq);         }     } }

---
+++ Infinite.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n,sq;
+    //scanf("%d",&n);
+    while(n!=-1)
+    {
+        scanf("%d",&n);
+        if(n!=-1)
+        {
+        sq=n*n;
+        printf("%d
+",sq);
+        }
+    }
+}
