74348fdb9414cee2136754d15b13dc83e9a07738
Mrinmoydatta <147488441+Mrinmoydatta@users.noreply.github.com>
2023-12-26 01:36:44 +0530

    #include<stdio.h> #include<math.h> int main() {     int n;     scanf("%d",&n);     int arr[n],i;     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     int even_sum=0,odd_sum=0;     for(i=0;i<n;i++)     {         if(i%2==0)         {             even_sum=even_sum+arr[i];         }         else         {             odd_sum=odd_sum+arr[i];         }     }     printf("%d",abs(even_sum-odd_sum)); }

---
+++ Absolute_difference_b/w_sum_of_even_and_sum_of_odd_indexed_elements.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n],i;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int even_sum=0,odd_sum=0;
+    for(i=0;i<n;i++)
+    {
+        if(i%2==0)
+        {
+            even_sum=even_sum+arr[i];
+        }
+        else
+        {
+            odd_sum=odd_sum+arr[i];
+        }
+    }
+    printf("%d",abs(even_sum-odd_sum));
+}
