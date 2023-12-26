bf1c91d2d44dd688c8b19b4a9294edc479355b85
Mrinmoydatta <147488441+Mrinmoydatta@users.noreply.github.com>
2023-12-27 01:58:57 +0530

    #include<stdio.h> int main() {     int n,r,count=0;     scanf("%d",&n);     while(n)     {         r=n%10;         count++;         n/=10;     }     printf("%d",count); }

--- Find_the_Digits_Count.c
+++ Find_the_Digits_Count.c
@@ -1,14 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,count=0;
+    int n,r,count=0;
     scanf("%d",&n);
     while(n)
     {
-        int r=n%10;
+        r=n%10;
         count++;
         n/=10;
     }
     printf("%d",count);
-    
 }
