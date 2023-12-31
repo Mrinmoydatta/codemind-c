291fe300279fb9de8de5c31b52396fef4964503a
Mrinmoydatta <147488441+Mrinmoydatta@users.noreply.github.com>
2023-12-31 19:04:41 +0530

    #include<stdio.h> int main() {     int n,i,j,k;     scanf("%d",&n);     int a=n;     for(i=1;i<=n;i++)     {         for(k=1;k<=i-1;k++)         {             printf(" ");         }         for(j=1;j<=a;j++)         {             printf("* ");         }         a--;         printf(" ");     } }

---
+++ Pattern_7.1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,k;
+    scanf("%d",&n);
+    int a=n;
+    for(i=1;i<=n;i++)
+    {
+        for(k=1;k<=i-1;k++)
+        {
+            printf(" ");
+        }
+        for(j=1;j<=a;j++)
+        {
+            printf("* ");
+        }
+        a--;
+        printf("
+");
+    }
+}
